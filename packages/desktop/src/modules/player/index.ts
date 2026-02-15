97b30383c7e8cfe579a4b859e254dc165e8258c9
lyswhut <lyswhut@qq.com>
2026-02-15 13:55:47 +0800

    fix: reflect playback queue management issue fix

--- packages/desktop/src/modules/player/index.ts
+++ packages/desktop/src/modules/player/index.ts
@@ -2,10 +2,12 @@ import { appEvent, appState } from '@/app'
 import { workers } from '@/worker'
 import { musicListEvent, sendMusicListAction } from '@any-listen/app/modules/musicList'
 import {
+  getPlayMusicInfo,
   initPlayer as initPlayerModule,
   playerEvent,
   setPlayInfo,
   setPlayMusic,
+  setPlayMusicInfo,
   setPlayTime,
 } from '@any-listen/app/modules/player'
 import { LIST_IDS } from '@any-listen/common/constants'
@@ -73,8 +75,9 @@ export const initPlayer = async () => {
   let prevCollectStatus = false
   playerEvent.on('musicChanged', async (index, historyIndex) => {
     void setPlayMusic(index, historyIndex)
-
+    const prevMusic = getPlayMusicInfo()
     const targetMusic = await getPlayerMusic()
+    setPlayMusicInfo(targetMusic)
     if (targetMusic) {
       void updateLatestPlayList(targetMusic)
       void checkCollect(targetMusic).then((isCollect) => {
@@ -86,6 +89,9 @@ export const initPlayer = async () => {
       // workers.dbService.playCountAdd({ name: mInfo.name, singer: mInfo.singer })
       // await musicListEvent.list_update_play_count(targetMusic.listId, targetMusic.musicInfo.name, targetMusic.musicInfo.singer)
       // workers.dbService.updateMetadataPlayCount()
+    }
+    if (prevMusic?.playLater) {
+      void playerEvent.playListAction({ action: 'remove', data: [prevMusic.itemId] })
     }
   })
   playerEvent.on('playInfoUpdated', (info) => {
