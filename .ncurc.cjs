2dbf0276526006b421cd1b95298df7ac26ffb3b8
lyswhut <lyswhut@qq.com>
2026-06-23 12:54:31 +0800

    chore: update deps

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,7 +1,7 @@
-const minor = ['electron', '@types/node', '@eslint/js', 'eslint', 'undici']
+const minor = ['@types/node', '@eslint/js', 'eslint', 'undici']
 const newest = ['oxfmt']
 const patch = []
-const ignore = ['@types/ws']
+const ignore = ['@types/ws', 'electron']
 
 const cooldown = '8h'
 
